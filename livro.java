3e94204561ef2d99b5f74bc7043ce441e83a4ce7
Jairo Torres <105895393+ojairotorres@users.noreply.github.com>
2024-10-31 12:11:07 -0300

    Add files via upload

---
+++ livro.java
@@ -0,0 +1,32 @@
+package org.acme.nts;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "Livros")
+public class livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String titulo;
+
+    @JoinColumn(name = "id_autorname", nullable = false)
+    @ManyToOne
+    private Autor_ autor;
+
+
+    public livro(){}
+
+
+    public livro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+}
